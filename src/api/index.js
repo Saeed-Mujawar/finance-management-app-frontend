21c45cf98f3b4f1f5ccba94ac302357b13400d14
Saeed-Mujawar <saeedmujawar20@gmail.com>
2024-08-22 18:51:32 +0530

    api-url changed commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://finance-management-app-backend-lczn.onrender.com';
 
 const api = axios.create({
     baseURL: API_URL,
